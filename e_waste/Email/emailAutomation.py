eaa323c91c1322ac30e0eb159ac272e21ab8677f
Kwan-CH <chkwan1120@gmail.com>
2025-04-01 10:52:44 +0800

    update email sending with html template

--- e_waste/Email/emailAutomation.py
+++ e_waste/Email/emailAutomation.py
@@ -1,6 +1,9 @@
+import base64
+
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
+
 import threading
 
 def sendEmail(email_type, recipient_email, context):
@@ -8,11 +11,11 @@ def sendEmail(email_type, recipient_email, context):
         email_templates = {
             'password_reset': {
                 'subject': "Password Reset Request",
-                'template': 'email/resetPassword.html'
+                'template': 'email/passwordReset.html'
             },
             'driver_assignment': {
                 'subject': "Your Assigned Driver Details",
-                'template': 'email/driverDetail.html'
+                'template': 'email/driverDetails.html'
             }
         }
 
@@ -21,6 +24,10 @@ def sendEmail(email_type, recipient_email, context):
         if not email_config:
             raise ValueError("Invalid email type provided.")
 
+        # image_path = "Email/static/images/companylogo.jpg"
+        # with open(image_path, "rb") as img:
+        #     base64_image = base64.b64encode(img.read()).decode("utf-8")  # Convert to Base64
+
         # Render the HTML template with dynamic data
         html_content = render_to_string(email_config['template'], context)
         text_content = strip_tags(html_content)  # Fallback plain text email
@@ -28,6 +35,7 @@ def sendEmail(email_type, recipient_email, context):
         # Create email message
         email = EmailMultiAlternatives(email_config['subject'], text_content, 'your-email@gmail.com', [recipient_email])
         email.attach_alternative(html_content, "text/html")  # Attach HTML version
+
         email.send()
 
     threading.Thread(target=send).start()
