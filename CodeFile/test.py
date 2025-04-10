6103db6daa0b3bf6e9830d5403368eabe8bfb799
Kwan-CH <153471297+Kwan-CH@users.noreply.github.com>
2025-04-10 09:31:26 +0800

    Update test.py
    removed commented API key

--- CodeFile/test.py
+++ CodeFile/test.py
@@ -1,29 +1,3 @@
-# import requests
-#
-#
-# url = "https://api.countrystatecity.in/v1/countries/MY/states"
-#
-# headers = {
-#   'X-CSCAPI-KEY': 'S1FOTHE5OW56bXpRWDNVQmpwckhZMFliYXgyNU9nUkZKRm1DRlc5bA=='
-# }
-#
-# response = requests.request("GET", url, headers=headers).json()
-#
-# state_code = []
-# for data in response:
-#     state_code.append(data.get('iso2'))
-#     print(data.get('name'), data.get('iso2'))
-#
-# for state_city in state_code:
-#     if state_city=='14':
-#         print('\n\n')
-#         url = f"https://api.countrystatecity.in/v1/countries/MY/states/{state_city}/cities"
-#         response = requests.request("GET", url, headers=headers).json()
-#         for data in response:
-#             print(data)
-#     # for area in response:
-#     #     print(area.get('name'))
-
 import os
 
 def print_tree(startpath, level=0):
